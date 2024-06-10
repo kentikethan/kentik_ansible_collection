3fdb85b0417934b2928bf693b1ee74890380f69d
Susan Hooks <susanhooks@gmail.com>
2024-06-10 20:07:15 +0000

    update for sites

--- playbooks/library/kentik_site.py
+++ playbooks/library/kentik_site.py
@@ -2,10 +2,11 @@
 
 # Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
-DOCUMENTATION = r'''
+DOCUMENTATION = r"""
 ---
 module: kentik_site
 
@@ -53,9 +54,9 @@ options:
 
 author:
     - Ethan Angele (@kentikethan)
-'''
+"""
 
-EXAMPLES = r'''
+EXAMPLES = r"""
 # Pass in a message
 - name: Create a Site
   kentik_site:
@@ -79,9 +80,9 @@ EXAMPLES = r'''
 - name: Test failure of the module
   create_kentik_site:
     title: fail me because site type not included
-'''
+"""
 
-RETURN = r'''
+RETURN = r"""
 # These are examples of possible return values, and in general should use other names for return values.
 original_message:
     description: The original name param that was passed in.
@@ -93,7 +94,7 @@ message:
     type: str
     returned: always
     sample: 'goodbye'
-'''
+"""
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_text
@@ -101,16 +102,19 @@ import requests
 import json
 import os
 
+
 def buildPayload(module):
     payload = module.params
-    del payload['email']
-    del payload['token']
-    del[payload['state']]
-    del[payload['region']]
+    del payload["email"]
+    del payload["token"]
+    del [payload["state"]]
+    del [payload["region"]]
     return payload
 
-def gatherSites(base_url,api_version,auth,module):
+
+def gatherSites(base_url, api_version, auth, module):
     url = f"{base_url}{api_version}/sites"
+
     payload = {}
     headers = auth
     try:
@@ -119,12 +123,13 @@ def gatherSites(base_url,api_version,auth,module):
     except ConnectionError as exc:
         module.fail_json(msg=to_text(exc))
     site_dict = {}
-    for site in site_data['sites']:
-        site_dict[site['title']] = site['id']
+    for site in site_data["sites"]:
+        site_dict[site["title"]] = site["id"]
     return site_dict
 
+
 def compareSite(site_list, module):
-    site=module.params["title"]
+    site = module.params["title"]
     if site in site_list:
         print("Site exists")
         return site_list[site]
@@ -132,7 +137,8 @@ def compareSite(site_list, module):
         print("Site does not exists")
         return False
 
-def deleteSite(base_url,api_version,auth,site_id,module):
+
+def deleteSite(base_url, api_version, auth, site_id, module):
     print("Deleting Site...")
     url = f"{base_url}{api_version}/sites/{site_id}"
     payload = {}
@@ -146,54 +152,69 @@ def deleteSite(base_url,api_version,auth,site_id,module):
     except ConnectionError as exc:
         module.fail_json(msg=to_text(exc))
 
-def createSite(base_url,api_version,auth,site_object,module):
+
+def createSite(base_url, api_version, auth, site_object, module):
     print("Creating Site...")
     url = f"{base_url}{api_version}/sites"
 
-    payload = json.dumps({
-        "site": site_object
-        })
+    payload = json.dumps({"site": site_object})
     headers = auth
     try:
         response = requests.request("POST", url, headers=headers, data=payload)
         if response.status_code == 200:
             site_data = response.json()
-            return site_data['site']['id']
+            return site_data["site"]["id"]
         else:
             module.fail_json(msg=response.text)
     except ConnectionError as exc:
         module.fail_json(msg=to_text(exc))
 
+
 def main():
     argument_spec = dict(
-        title=dict(type='str', required=True),
-        postalAddress=dict(type='dict', required=False),
-        type=dict(choices=["SITE_TYPE_DATA_CENTER","SITE_TYPE_CLOUD","SITE_TYPE_BRANCH","SITE_TYPE_CONNECTIVITY","SITE_TYPE_CUSTOMER","SITE_TYPE_OTHER"], required=False, default="SITE_TYPE_OTHER"),
-        lat=dict(type="int", required=False),
-        lon=dict(type="int", required=False),
-        email=dict(type='str', required=False, default=os.environ['KENTIK_EMAIL']),
-        token=dict(type='str', no_log=True, required=False, default=os.environ['KENTIK_TOKEN']),
+        title=dict(type="str", required=True),
+        postalAddress=dict(type="dict", required=False),
+        type=dict(
+            choices=[
+                "SITE_TYPE_DATA_CENTER",
+                "SITE_TYPE_CLOUD",
+                "SITE_TYPE_BRANCH",
+                "SITE_TYPE_CONNECTIVITY",
+                "SITE_TYPE_CUSTOMER",
+                "SITE_TYPE_OTHER",
+            ],
+            required=False,
+            default="SITE_TYPE_OTHER",
+        ),
+        lat=dict(type="float", required=False),
+        lon=dict(type="float", required=False),
+        email=dict(type="str", required=False, default=os.environ["KENTIK_EMAIL"]),
+        token=dict(
+            type="str", no_log=True, required=False, default=os.environ["KENTIK_TOKEN"]
+        ),
         state=dict(default="present", choices=["present", "absent"]),
-        region=dict(type='str', default="US", choices=["US", "EU"])
+        region=dict(type="str", default="US", choices=["US", "EU"]),
+        siteMarket=dict(type="str", required=False),
     )
     module = AnsibleModule(
         argument_spec=argument_spec,
+        supports_check_mode=True,
     )
     result = {"changed": False}
     warnings = list()
-    state = module.params['state']
+    state = module.params["state"]
     auth = {
-        'X-CH-Auth-Email': module.params['email'], 
-        'X-CH-Auth-API-Token': module.params['token'], 
-        'Content-Type': 'application/json'
-        }
-    if module.params['region'] == "EU":
+        "X-CH-Auth-Email": module.params["email"],
+        "X-CH-Auth-API-Token": module.params["token"],
+        "Content-Type": "application/json",
+    }
+    if module.params["region"] == "EU":
         base_url = "https://grpc.api.kentik.eu/site/"
     else:
         base_url = "https://grpc.api.kentik.com/site/"
     api_version = "v202211"
     site_object = buildPayload(module)
-    site_list = gatherSites(base_url,api_version,auth,module)
+    site_list = gatherSites(base_url, api_version, auth, module)
     site_exists = compareSite(site_list, module)
 
     if site_exists:
@@ -201,16 +222,17 @@ def main():
             result["changed"] = False
             result["site_id"] = site_exists
         elif state == "absent":
-            deleteSite(base_url,api_version,auth,site_exists,module)
+            deleteSite(base_url, api_version, auth, site_exists, module)
             result["changed"] = True
     else:
         if state == "present":
-            site_id = createSite(base_url,api_version,auth,site_object,module)
+            site_id = createSite(base_url, api_version, auth, site_object, module)
             result["changed"] = True
             result["site_id"] = site_id
         elif state == "absent":
             result["changed"] = False
     module.exit_json(**result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
